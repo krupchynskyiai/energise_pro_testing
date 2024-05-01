81f5cf7a5229f8048490c82b2b75fff3c6fae740
krupchynskyai <krupchynskyi.plbl@gmail.com>
2024-05-01 23:04:42 +0300

    maps + tabs

--- lib/pages/ip_location_page/ip_location_page.dart
+++ lib/pages/ip_location_page/ip_location_page.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http/http.dart' as http;
 import 'package:get_ip_address/get_ip_address.dart';
+
+import 'package:latlong2/latlong.dart';
 
 class IPLocation extends StatefulWidget {
   IPLocation({super.key});
@@ -12,6 +16,7 @@ class IPLocation extends StatefulWidget {
 
 class IPLocationState extends State<IPLocation> {
   var fetchResult;
+  bool isLoading = true;
 
   void initState() {
     super.initState();
@@ -20,13 +25,13 @@ class IPLocationState extends State<IPLocation> {
   }
 
   void main() async {
+    print('here');
     try {
       /// Initialize Ip Address
-      var ipAddress = IpAddress(type: RequestType.json);
+      final ipAddress = IpAddress(type: RequestType.json);
 
       /// Get the IpAddress based on requestType.
       dynamic data = await ipAddress.getIpAddress();
-      print(data['ip']);
       fetchData(data['ip']);
     } on IpAddressException catch (exception) {
       /// Handle the exception.
@@ -35,14 +40,85 @@ class IPLocationState extends State<IPLocation> {
   }
 
   Future fetchData(ipAddress) async {
-    print('fetching started');
     final response =
         await http.get(Uri.parse('http://ip-api.com/json/$ipAddress'));
-    print('http://ip-api.com/json/$ipAddress');
-    print(jsonDecode(response.body)['country']);
+    fetchResult = jsonDecode(response.body);
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<void> _refresh() {
+    main();
+    return Future.delayed(Duration(seconds: 2));
   }
 
   Widget build(BuildContext context) {
-    return Text('Response ');
+    return isLoading
+        ? Text('Loading')
+        : Container(
+            height: 500,
+            width: double.infinity,
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 250,
+                      child: FlutterMap(
+                        options: MapOptions(
+                          initialCenter:
+                              LatLng(fetchResult['lat'], fetchResult['lon']),
+                          initialZoom: 9.2,
+                        ),
+                        children: [
+                          TileLayer(
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            userAgentPackageName: 'com.example.app',
+                          ),
+                          MarkerLayer(
+                            markers: [
+                              Marker(
+                                point: LatLng(
+                                    fetchResult['lat'], fetchResult['lon']),
+                                width: 50,
+                                height: 50,
+                                child: SvgPicture.asset(
+                                    'assets/icons/geolocation.svg'),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Text('Country - ${fetchResult['country']}'),
+                    Text('Country Code - ${fetchResult['countryCode']}'),
+                    Text('Region - ${fetchResult['region']}'),
+                    Text('Country Code - ${fetchResult['regionName']}'),
+                    Text('Country Code - ${fetchResult['city']}'),
+                    Text('Country Code - ${fetchResult['zip']}'),
+                    Text('Country Code - ${fetchResult['lat']}'),
+                    Text('Country Code - ${fetchResult['lon']}'),
+                    Text('Country Code - ${fetchResult['timezone']}'),
+                    Text('Country Code - ${fetchResult['isp']}'),
+                    Text('Country Code - ${fetchResult['org']}'),
+                    Text('Country Code - ${fetchResult['as']}'),
+                    Text('Country Code - ${fetchResult['query']}'),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isLoading = true;
+                          });
+
+                          main();
+                        },
+                        child: Text('Refresh'))
+                  ],
+                ),
+              ),
+            ));
   }
 }
